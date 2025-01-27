ec599855d6235bba033abdcfd531a72064464713
Khodor Harakeh <150835475+Kh-Harakeh@users.noreply.github.com>
2025-01-27 22:14:29 +0330

    Create print_formatting.py

---
+++ 1.Inputs,Outputs,Variables/print_formatting.py
@@ -0,0 +1,15 @@
+#padding
+print('%03d'%(2))
+
+#f-string
+hour = 9
+minute = 6
+city = 'Gotham'
+temperature = 13.96789
+
+print(f'Location: {city}')
+print(f'Time: {hour:02d}:{minute:02d}')
+print(f'Temperature: {temperature:.2f}°C')
+
+#type
+print(type(hour)) #<class 'int'>
